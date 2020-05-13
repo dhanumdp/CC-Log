177f8e2a4a5526e3815b14071bef4a8964d5a7ca
Dhanu <dhanumca3748@gmail.com>
2020-05-13 13:50:46 +0530

    Backend API Over

--- adminRoutes.js
+++ adminRoutes.js
@@ -5,43 +5,13 @@ var mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
 const crypto = require('crypto').randomBytes(256).toString('hex');
+var nodemailer = require('nodemailer');
 
-router.post('/addAdmin',(req,res)=>{
-    var collection = mongoose.connection.db.collection('Admins');
-    let user={
-        Username : req.body.username,
-        Password : req.body.Password
-    }
-               
-    collection.find({'Username':user.Username}).count((error,us)=>{
-                        if(us !=0)
-                        {
-                            //res.json('Already one User registered with this '+user.Email);
-                            res.send({success:false, message : 'User Exists'})
-                        }
-                        else
-                        {
-                            collection.insertOne(user,(error,userDetail)=>{
-                                if(!error)
-                                {
-                                    res.send({success:true, message : user.Username+' Registered Successfully'})
-                        
 
-                                    console.log(user.Username+' Registered Successfully');
-                                   // console.log(userDetail);
-                                }
-                                else
-                                {
-                                    res.send({success:false , message : "Registration Error"})
-                                    console.log('Registration Error'+err);
-                                }
-                        })
-                        }
-                })
-            
-});
+const {Complaints} = require('./complaint');
 
-//student login
+
+//admin login
 
 router.post('/login',(req,res)=>{
 
@@ -78,4 +48,211 @@ collection.find({'Username':username}).count((err,num)=>{
 });
 
 
+//adding admin
+router.post('/addAdmin',(req,res)=>{
+    var collection = mongoose.connection.db.collection('Admins');
+    let user={
+        Username : req.body.username,
+        Password : req.body.password
+    }
+               
+    collection.find({'Username':user.Username}).count((error,us)=>{
+                        if(us !=0)
+                        {
+                            //res.json('Already one User registered with this '+user.Email);
+                            res.send({success:false, message : 'User Exists'})
+                        }
+                        else
+                        {
+                            collection.insertOne(user,(error,userDetail)=>{
+                                if(!error)
+                                {
+                                    res.send({success:true, message : user.Username+' Registered Successfully'})
+                        
+
+                                    console.log(user.Username+' Registered Successfully');
+                                   // console.log(userDetail);
+                                }
+                                else
+                                {
+                                    res.send({success:false , message : "Registration Error"})
+                                    console.log('Registration Error'+err);
+                                }
+                        })
+                        }
+                })
+            
+});
+
+//changing password
+
+router.post('/changePassword',(req,res)=>{
+
+    var collection = mongoose.connection.db.collection('Admins');
+    console.log(req.body)
+    collection.findOneAndUpdate({'Username':req.body.username},{$set :{'Password':req.body.password} }).then(()=>{
+        res.send({success:true, message : 'Password Changed Successfully'})
+    }).catch((err)=>{
+        res.send({success:false, message:"Password Not Changed"})
+    })
+
+})
+
+
+
+
+
+//get Attendance
+
+router.post('/getAttendanceList',(req,res)=>{
+    var collection = mongoose.connection.db.collection('Attendance');
+    collection.find({'date':req.body.date}).count((err,num)=>{
+        if(num == 0)
+        {
+            res.send({success:false, message: 'No Records Found'})
+        }
+        else
+        {
+           
+
+            collection.find({'date':req.body.date}).toArray((error,docs)=>{
+                if(error)
+                {
+                    res.send({success:false, message : 'Error While Fetching Attendance From DB'})
+                }
+                else
+                {
+                    res.send({success:true, data : docs})
+                }
+            })
+        }
+    })
+    
+})
+
+//get Complaints
+
+router.post('/getComplaintList',(req,res)=>{
+    var collection = mongoose.connection.db.collection('Complaints');
+ 
+        
+            collection.find({}).toArray((error,docs)=>{
+                if(error)
+                {
+                    res.send({success:false, message : 'Error While Fetching Complaints From DB'})
+                }
+                else
+                {
+                    res.send({success:true, data : docs})
+                }
+            })
+
+    
+})
+
+
+//sending mail to students
+
+
+function mailsend(userDetail){
+ 
+    const transporter = nodemailer.createTransport({
+        
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,  //true for 465 port, false for other ports
+        auth: {
+          user: 'cclabmca@gmail.com',
+          pass: '18mxians'
+        }
+      });
+               
+      const mailOptions = {
+        from: 'cclabmca@gmail.com', // sender address
+        to: userDetail.Mail, // list of receivers
+        subject: "MXCC - MCA CC LAB COMPLAINT", 
+        html:"<h4> Hai "+userDetail.Name+". "+userDetail.Message+"</h4> <br><h3><i>"+userDetail.Message1+"</i></h3>"
+    };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+             console.log(error);
+             //  res.status(400).send(error);
+        } else {
+             console.log('Success');
+             // res.status(200).send(info);
+        }
+      });
+    
+}
+
+//update Complaint Status
+router.get('/updateComplaint/:id',(req,res)=>{
+
+var collection = mongoose.connection.db.collection('Users');
+    
+    Complaints.findById({_id:req.params.id}).then((doc)=>{
+        //console.log(doc);
+        var rollno = doc.rollno;
+        var complaint = doc.complaint;
+        var systemNo = doc.systemNo;
+        Complaints.findByIdAndUpdate({_id:req.params.id},{$set:{status:'Solved Issue'}}).then(()=>{
+            res.send({success:true, message:'Status Updated'})
+
+            collection.findOne({'Roll_No':rollno}).then((user)=>{
+                let UserDetail ={       
+                    Name : user.Name,
+                    Mail :user.Email,
+                    Message : "The Issue you mentioned "+complaint+" on "+systemNo+" is Solved.",
+                    Message1 :"Thank You"
+                }
+                mailsend(UserDetail);
+            }).catch((err)=>{
+                res.send({success:false, message :'Error while finding Student Mail'})
+            })
+           
+        }).catch((error=>{
+            res.send({success:false, message:'Status Updation Failed'})
+        }))
+                
+        }).catch((err)=>{
+            res.send({success:false, message:'Error While Retrieving Complaint'})
+    })
+})
+//delete Complaint
+router.get('/deleteComplaint/:id',(req,res)=>{
+
+    var collection = mongoose.connection.db.collection('Users');
+        
+        Complaints.findById({_id:req.params.id}).then((doc)=>{
+            //console.log(doc);
+            var rollno = doc.rollno;
+            var complaint = doc.complaint;
+            var systemNo = doc.systemNo;
+            Complaints.findByIdAndDelete({_id:req.params.id}).then(()=>{
+                res.send({success:true, message:'Complaint Deleted'})
+                collection.findOne({'Roll_No':rollno}).then((user)=>{
+                    let UserDetail ={       
+                        Name : user.Name,
+                        Mail :user.Email,
+                        Message : "The Issue you mentioned "+complaint+" on "+systemNo+" is Deleted.",
+                        Message1: "If you received this email before getting Solved Issue Mail, it means your complaint is inappropriate. So that it will be deleted. "
+
+                    }
+                    mailsend(UserDetail);
+                }).catch((err)=>{
+                    res.send({success:false, message :'Error while finding Student Mail'})
+                })
+               
+            }).catch((error=>{
+                res.send({success:false, message:'Complait Deletion Failed'})
+            }))
+                    
+            }).catch((err)=>{
+                res.send({success:false, message:'Error While Retrieving Complaint'})
+        })
+    })
+
+      
+
 module.exports=router;
+
